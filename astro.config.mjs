9150a675dc1d4dc827ca04d29979a32ccc5bdb89
Fraser Wilkinson <fraserwilk@users.noreply.github.com>
2024-09-09 12:04:03 +1000

    fixing Not found (404) errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,16 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  redirects: {
+    '/faq': '/about',
+    '/index.php': '/',
+    '/home': '/',
+    '/terms-and-conditions': '/info/terms',
+    '/index.php/terms-and-conditions': '/info/terms',
+    '/privacy-statement': '/info/privacy',
+    '/privacy/': '/info/privacy',
+    '/index.php/faq': '/about',
+    '/sendContactForm': '/contact'
+  }
 });
