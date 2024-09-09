8fb2bc2ecb7455ea7a6e22282031f942b78d27e3
Fraser Wilkinson <fraserwilk@users.noreply.github.com>
2024-09-09 12:10:32 +1000

    sitemap update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
   redirects: {
     '/faq': '/about',
     '/index.php': '/',
