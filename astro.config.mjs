d2eec47d8fdfcf2a5eda8528292ac7ec609e2b23
Fraser Wilkinson <fraserwilk@users.noreply.github.com>
2024-09-11 10:20:22 +1000

    more @layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@layouts': '/src/layouts',
+      },
+    },
+  },
   integrations: [icon(), sitemap()],
   redirects: {
     '/faq': '/about',
